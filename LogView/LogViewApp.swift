929379411d478e351e74c3a8fd59213b8f80309a
Michał Plutecki <michal.piotr.plutecki@gmail.com>
2022-11-19 14:42:07 +0100

    Initial Commit

---
+++ LogView/LogViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogViewApp.swift
+//  LogView
+//
+//  Created by Michał Plutecki on 19/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct LogViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
