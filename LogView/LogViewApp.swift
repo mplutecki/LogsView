d69656a752928272218e36115c6ecf7b6d190e6b
Michał Plutecki <michal.piotr.plutecki@gmail.com>
2022-11-19 23:26:37 +0100

    Add log category filter

--- LogView/LogViewApp.swift
+++ LogView/LogViewApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct LogViewApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LogsView()
         }
     }
 }
--- LogView/LogsView.swift
+++ LogView/LogsView.swift
@@ -1,16 +1,14 @@
 import SwiftUI
 
-struct ContentView: View {
+struct LogsView: View {
 	@State var searchText = ""
 	@State var showCategories = false
-	@State private var multiSelection = Set<UUID>()
+	@State var selectedCategories = Set<String>()
 	
 	var body: some View {
 		NavigationView {
-			List {
-				ForEach(messages, id: \.self) { message in
-					logMessage(message)
-				}
+			List(messages, id: \.self) { message in 
+				logMessage(message)
 			}
 			.searchable(text: $searchText, placement: .automatic)
 			.navigationTitle("Logs")
@@ -19,11 +17,7 @@ struct ContentView: View {
 			.font(.system(size: 14, weight: .semibold, design: .monospaced))
 			.toolbar {
 				ToolbarItem(placement: .navigationBarLeading) { 
-					Button { 
-						showCategories = true
-					} label: { 
-						Image(systemName: "line.3.horizontal.decrease.circle")
-					}
+					filterButton
 				}
 				ToolbarItem(placement: .navigationBarTrailing) { 
 					Button { 
@@ -34,26 +28,9 @@ struct ContentView: View {
 				}
 			}
 			.sheet(isPresented: $showCategories) {
-				NavigationView { 
-					List(selection: $multiSelection) { 
-						Text("UI")
-						Text("UI")
-						Text("UI")
-					}
-					.listStyle(.insetGrouped)
-					.toolbar { 
-						ToolbarItem(placement: .navigationBarLeading) { 
-							Button("Clear", action: {})
-						}
-						ToolbarItem(placement: .navigationBarTrailing) { 
-							Button("Close", action: {
-								showCategories = false
-							})
-						}
-					}
-					.navigationTitle("Filter")
-					.navigationBarTitleDisplayMode(.inline)
-				}
+				FilterView(selectedCategories: $selectedCategories, onClose: {
+					showCategories = false
+				})
 			}
 		}
 	}
@@ -69,11 +46,23 @@ struct ContentView: View {
 			Text(message)
 		}
 	}
+	
+	var filterButton: some View {
+		Button { 
+			showCategories = true
+		} label: {
+			if selectedCategories.isEmpty {
+				Image(systemName: "line.3.horizontal.decrease.circle")	
+			} else {
+				Image(systemName: "line.3.horizontal.decrease.circle.fill")
+			}
+		}
+	}
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct LogsView_Previews: PreviewProvider {
 	static var previews: some View {
-		ContentView()
+		LogsView()
 	}
 }
 
