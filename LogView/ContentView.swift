4b7f283f8ba6a18a454e9884cd7069d1e74eba92
Michał Plutecki <michal.piotr.plutecki@gmail.com>
2022-11-19 22:48:57 +0100

    Basic list for displaying logs

--- LogView/ContentView.swift
+++ LogView/ContentView.swift
@@ -1,26 +1,124 @@
-//
-//  ContentView.swift
-//  LogView
-//
-//  Created by Michał Plutecki on 19/11/2022.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+	@State var searchText = ""
+	@State var showCategories = false
+	@State private var multiSelection = Set<UUID>()
+	
+	var body: some View {
+		NavigationView {
+			List {
+				ForEach(messages, id: \.self) { message in
+					logMessage(message)
+				}
+			}
+			.searchable(text: $searchText, placement: .automatic)
+			.navigationTitle("Logs")
+			.navigationBarTitleDisplayMode(.inline)
+			.listStyle(.plain)
+			.font(.system(size: 14, weight: .semibold, design: .monospaced))
+			.toolbar {
+				ToolbarItem(placement: .navigationBarLeading) { 
+					Button { 
+						showCategories = true
+					} label: { 
+						Image(systemName: "line.3.horizontal.decrease.circle")
+					}
+				}
+				ToolbarItem(placement: .navigationBarTrailing) { 
+					Button { 
+						
+					} label: { 
+						Image(systemName: "arrow.clockwise")
+					}
+				}
+			}
+			.sheet(isPresented: $showCategories) {
+				NavigationView { 
+					List(selection: $multiSelection) { 
+						Text("UI")
+						Text("UI")
+						Text("UI")
+					}
+					.listStyle(.insetGrouped)
+					.toolbar { 
+						ToolbarItem(placement: .navigationBarLeading) { 
+							Button("Clear", action: {})
+						}
+						ToolbarItem(placement: .navigationBarTrailing) { 
+							Button("Close", action: {
+								showCategories = false
+							})
+						}
+					}
+					.navigationTitle("Filter")
+					.navigationBarTitleDisplayMode(.inline)
+				}
+			}
+		}
+	}
+	
+	func logMessage(_ message: String) -> some View {
+		VStack(alignment: .leading, spacing: 4) {
+			HStack {
+				Text("13:33:33")	
+				Text("[UI]")
+			}
+			.font(.system(size: 12, design: .monospaced))
+			.foregroundColor(.gray)
+			Text(message)
+		}
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		ContentView()
+	}
 }
+
+let messages = [
+	"Message",
+	"Message",
+	"Message",
+	"Message",
+	"Message",
+	"Message",
+	"Message",
+	"Message",
+	"Message",
+	"Message",
+	"Message",
+	"Message",
+	"Message",
+	"Message",
+	"Message",
+	"Message",
+	"Message",
+	"Message",
+	"Message",
+	"Message",
+	"Message",
+	"Message",
+	"Message",
+	"Message",
+	"Message",
+	"Message",
+	"Message",
+	"Message",
+	"Message",
+	"Message",
+	"Message",
+	"Message",
+	"Message",
+	"Message",
+	"Message",
+	"Message",
+	"Message",
+	"Message",
+	"Message",
+	"Message",
+	"Message",
+	"Message",
+	"Message"
+]
